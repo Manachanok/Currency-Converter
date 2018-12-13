81fb395efe34a7146f2a930d295807a05c84efca
Manachanok <42858479+Manachanok@users.noreply.github.com>
2018-12-14 01:51:18 +0700

    Update GUIForm.java

--- GUIForm.java
+++ GUIForm.java
@@ -1,7 +1,6 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Developer :: Tummanoon  Kitcharas-anan	  6001012630047
+                Manachanok  Sirivibulkovit	  6001012630101	
  */
 package ForCon;
 
